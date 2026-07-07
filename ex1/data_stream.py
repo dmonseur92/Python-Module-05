bcf657ed25ad0d0c97fe8c2f640e45dad032cd42
Denis <denismonseur1992@gmail.com>
2026-07-07 18:50:07 +0200

    holymoly

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -19,11 +19,21 @@ class DataStream():
                     sucess = True
                     break
             if not sucess:
-                print(f"DataStream error - Can't process element in stream: {item}")
+                print(f"DataStream error - "
+                      f"Can't process element in stream: {item}")
+
+    def print_processors_stats(self) -> None:
+        for processor in self.processors:
+            print(f"{processor.__class__.__name__}:"
+                  f"total {processor.total_processed}"
+                  "items processed, remaining "
+                  f"{len(processor.storage)} on processor")
+
 
 class DataProcessor(ABC):
     def __init__(self) -> None:
         self.storage: list[tuple[int, str]] = []
+        self.total_processed = 0
 
     @abstractmethod
     def validate(self, data: Any) -> bool:
@@ -58,7 +68,9 @@ class NumericProcessor(DataProcessor):
             for x in data:
                 self.storage.append((self.counter, str(x)))
                 self.counter += 1
+                self.total_processed += 1
         else:
+            self.total_processed += 1
             self.storage.append((self.counter, str(data)))
             self.counter += 1
 
@@ -80,7 +92,9 @@ class TextProcessor(DataProcessor):
             for x in data:
                 self.storage.append((self.counter, x))
                 self.counter += 1
+                self.total_processed += 1
         else:
+            self.total_processed += 1
             self.storage.append((self.counter, data))
             self.counter += 1
 
@@ -116,7 +130,9 @@ class LogProcessor(DataProcessor):
             for log in data:
                 self.storage.append((self.counter, format_log(log)))
                 self.counter += 1
+                self.total_processed += 1
         else:
+            self.total_processed += 1
             self.storage.append((self.counter, format_log(data)))
             self.counter += 1
 
@@ -127,14 +143,21 @@ if __name__ == "__main__":
     txt = TextProcessor()
     log = LogProcessor()
     stream_data = [
-    "Hello world",
-    [3.14, -1, 2.71],
-    [
-        {"log_level": "WARNING", "log_message": "Telnet access! Use ssh instead"},
-        {"log_level": "INFO", "log_message": "User wil is connected"}
-    ],
-    42,
-    ["Hi", "five"]]
+        "Hello world",
+        [3.14, -1, 2.71],
+        [
+            {
+                "log_level": "WARNING",
+                "log_message": "Telnet access! Use ssh instead",
+            },
+            {
+                "log_level": "INFO",
+                "log_message": "User wil is connected",
+            },
+        ],
+        42,
+        ["Hi", "five"],
+    ]
     print("=== Code Nexus - Data Stream ===\n")
     print("Initialize Data Stream...")
     print("== DataStream statistics ==")
@@ -143,9 +166,22 @@ if __name__ == "__main__":
     stream.register_processor(num)
     print(f"Send first batch of data on stream: {stream_data}")
     stream.process_stream(stream_data)
-    # print(num.output())
-    # print(txt.output())
-    # print(log.output())
-
+    print("== DataStream statistics ==")
+    stream.print_processors_stats()
+    print()
+    print("Registering other data processors")
     stream.register_processor(txt)
     stream.register_processor(log)
+    print("Send the same batch again")
+    stream.process_stream(stream_data)
+    stream.print_processors_stats()
+    print()
+    print("Consume some elements from the data processors:"
+          " Numeric 3, Text 2, Log 1")
+    for _ in range(3):
+        num.output()
+    for _ in range(2):
+        txt.output()
+    log.output()
+    print("== DataStream statistics ==")
+    stream.print_processors_stats()
